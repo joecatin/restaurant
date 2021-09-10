92ec90cfa427d52e73f8757bf97fbd94ab40d9f1
bautheac <o.bautheac@gmail.com>
2021-09-10 14:31:46 +0100

    clean code

--- src/index.js
+++ src/index.js
@@ -14,37 +14,5 @@ const content = document.createElement('div');
 content.id = 'content-container';
 main.appendChild(content);
 
-load("nav").then((component) => {
-    const nav = document.getElementById('nav-container');
-    nav.appendChild(component());
-});
-load("cover").then((component) => {
-    const content = document.getElementById('content-container');
-    content.appendChild(component());
-});
-
-
-
-
-
-// async function getNav() {
-//     const { nav } = await import('./components/nav/component.js');
-//     return nav();
-// }
-
-// getNav().then((component) => {
-//     const main = document.getElementById('main-container');
-//     main.appendChild(component);
-// });
-
-// async function getContent(content) {
-//     // const { menu } = await import('./components/menu/menu.js');
-//     // return menu("drinks");
-//     const { about } = await import('./components/about/component.js');
-//     return about();
-// }
-
-// getContent().then((component) => {
-//     const main = document.getElementById('main-container');
-//     main.appendChild(component);
-// });
+load("nav").then(component => nav.appendChild(component()));
+load("cover").then(component => content.appendChild(component()));
