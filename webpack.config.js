19089023334e76e0eba76ff098eeacb927d693d7
bautheac <o.bautheac@gmail.com>
2021-09-10 15:09:41 +0100

    aesthetic

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
+  // mode: 'development',
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
